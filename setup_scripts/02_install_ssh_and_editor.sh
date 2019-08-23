b648af3c56c7d6dc61b75d7be157edaf865951ba
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-08-23 18:08:41 +0900

    delete folder

--- setup_scripts/02_install_ssh_and_editor.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-sudo apt-get install emacs -y
-sudo apt-get install openssh-server -y
--- setup_scripts/03_set_timezone_utc_and_install_ntp.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-sudo timedatectl set-timezone UTC
-
-sudo apt-get install ntp -y
-sudo cp $HOME/git/opust/config/etc-ntp.conf.osaka /etc/ntp.conf
-sudo systemctl restart ntp
-
-ntpq -p
