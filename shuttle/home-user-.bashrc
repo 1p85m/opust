9c282578554295c98c6619cb8cc99673ef5b0fb1
kotakayoshi <tatuyuki.takashima@gmail.com>
2019-11-09 11:26:56 +0900

    test

---
+++ shuttle/home-user-.bashrc
@@ -0,0 +1,17 @@
+
+# history
+# -------
+export HISTSIZE=9999
+export HISTFILESIZE=999999
+export HISTIGNORE=ls:history:pwd
+export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
+
+# ros
+# ---
+source /opt/ros/melodic/setup.bash
+source $HOME/ros/devel/setup.bash
+export PYTHONPATH=$PYTHONPATH:$HOME/ros/devel/lib/python2.7/dist-packages
+export ROS_PACKAGE_PATH=$HOME/ros/src:$ROS_PACKAGE_PATH
+export ROS_IP=192.168.100.61
+export ROS_HOSTNAME=192.168.100.61
+export ROS_MASTER_URI=http://192.168.100.190:11311
---
+++ shuttle/setup.sh
@@ -0,0 +1,100 @@
+#! /bin/sh
+
+# Required environment : Ubuntu 18.04 LTS
+# Comments:
+#   - execute this file by root user
+
+# prepare setup
+cd `dirname "${0}"`
+apt update
+apt upgrade -y
+
+# install applications
+## general
+apt install -y ntp
+apt install -y emacs
+apt install -y openssh-server
+apt install -y python3
+apt install -y python3-pip
+
+## for development
+apt install -y make
+apt install -y build-essential
+apt install -y libssl-dev
+apt install -y zlib1g-dev
+apt install -y libbz2-dev
+apt install -y llvm
+apt install -y libreadline-dev
+apt install -y libsqlite3-dev
+apt install -y libncurses5-dev
+apt install -y libncursesw5-dev
+apt install -y libpng-dev
+apt install -y catkin
+
+# general setup
+## ntp configurations
+timedatectl set-timezone UTC
+cp /etc/ntp.conf　/etc/ntp.conf.original
+cp etc-ntp.conf.osaka /etc/ntp.conf
+systemctl restart ntp
+
+## .bashrc configurations
+cp $HOME/.bashrc $HOME/.bashrc.`date '%Y%m%d_%H%M%S'`
+cat home-user-.bashrc >> $HOME/.bashrc
+
+
+# python setup
+## general
+pip3 install ipython
+pip3 install numpy
+pip3 install scipy
+pip3 install matplotlib
+pip3 install pandas
+pip3 install psutil
+
+## astro
+pip3 install aplpy
+pip3 install reproject
+pip3 install astroquery
+pip3 install wcsaxes
+pip3 install healpy
+
+## device driver
+pip3 install pyinterface
+pip3 install ogameasure
+pip3 install necstdb
+pip3 install xfftspy
+
+# ROS setup
+
+## ros
+pip3 install rospkg
+pip3 install catkin_pkg
+pip3 install empy
+
+cp -r /usr/local/lib/python3.6/dist-packages /usr/lib/python2.7/dist-packages
+
+## install ROS
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+apt update
+apt install -y ros-melodic-ros-base
+rosdep init
+rosdep update
+
+mkdir -p $HOME/ros/src
+cd $HOME/ros/src
+catkin_init_workspace
+
+## install necst packages
+### necst-core
+git clone https://github.com/ogawa-ros/necst-core.git $HOME/ros/src/necst-core
+git clone https://github.com/ogawa-ros/necst-sisrx.git $HOME/ros/src/necst-sisrx
+git clone https://github.com/ogawa-ros/necst-sisrx_dsb.git $HOME/ros/src/necst-sisrx_dsb
+git clone https://github.com/ogawa-ros/rospkg-ogameasure.git $HOME/ros/src/rospkg-ogameasure
+git clone https://github.com/ogawa-ros/necst-telescope.git
+git clone https://github.com/ogawa-ros/rospkg-pyinterface.git $HOME/ros/src/rospkg-pyinterface
+
+cd $HOME/ros
+catkin_make
